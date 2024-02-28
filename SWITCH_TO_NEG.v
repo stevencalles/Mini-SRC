f52f7c6992604ebdd4398d1f299bb19be485124f
coleMeeakings <161643805+coleMeeakings@users.noreply.github.com>
2024-02-28 14:48:19 -0500

    Add files via upload

---
+++ SWITCH_TO_NEG.v
@@ -0,0 +1,23 @@
+module SWITCH_TO_NEG(B, Result);
+
+input [31:0] B;
+output [31:0] Result;
+
+reg [31:0] Result;
+
+integer i, j, holder;
+
+always@(B) //what does A or B
+	begin
+		if(A[31]==1)
+		begin
+			Result=B ^ 32'b11111111111111111111111111111111;    //Xor operation with 1 effectively switch bit-wise
+			Result=Result+1;
+		end
+		else
+		begin
+			Result=B ^ 32'b11111111111111111111111111111111;	//Xor operation with 1 effectively switch bit-wise
+			Result=Result+1;
+		end	
+end
+endmodule
