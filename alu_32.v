f8a6bcf3f94555f0987eb1791b606efb104c20fa
coleMeeakings <161643805+coleMeeakings@users.noreply.github.com>
2024-02-28 18:53:23 -0500

    Update alu_32.v

--- alu_32.v
+++ alu_32.v
@@ -9,54 +9,80 @@ module alu_32(input InPC, input [31:0] A, B, input [4:0] opcode, output reg[31:0
 	Rol= 5'b01001,
 	And= 5'b01010,
 	Or= 5'b01011;
+	
+	Mul=5'b01111;
+	Div=5'b10000;
+	Not=5'b10010;
+	Neg=5'b10001;
 
 
-	wire [31:0] Add_out, Sub_out, Shr_out, Shra_out, Shl_out, Ror_out, Rol_out, And_out, Or_out, InPC_out, Not_out;
+	wire [31:0] Add_out, Sub_out, Shr_out, Shra_out, Shl_out, Ror_out, Rol_out, And_out, Or_out, InPC_out, Not_out, Neg_Result, lo, hi;
 
 	assign Not_out = ~A;
 	
 	// different ALU operations
+	mul mul(.A(A), .B(B), .lo(Lo), .hi(Hi);
 	adder	adder(.A(A), .B(B), .Add_out(Result));
-	subtracter subtracter(.A(A), .B(B), .Sub_out(Difference));
+	SWITCH_TO_NEG	SWITCH_TO_NEG(.B(B), .Neg_Result(Result));	//switch B to -B
+	adder subber(.A(A), .Neg_Result(B), .Sub_out(Result));			//add A+(-B)
 	and_32 and_32(.A(A), .B(B), .And_out(z));
+	SHR	SHR(.A(A), .B(B), .Shr_out(Result));
+	SHRA	SHRA(.A(A), .B(B), .Shra_out(Result));
+	SHL	SHL(.A(A), .B(B), .Shl_out(Result));
+	ROR	ROR(.A(A), .B(B), .Ror_out(Result));
+	ROL	ROL(.A(A), .B(B), .Rol_out(Result));
+	or_32	or_32(.A(A), .B(B), .or_out(z));
+	SWITCH_TO_NEG	A(.A(A), .Neg_A(Result));
 
 	always @ (*) begin
-	//	case (InPC)
-	//		1'b1: begin
-	//		LO <= 
-	//		end
-		case (opcode)
-//			Add: begin
-//				output_result = Add_out;
-//				end
-//			Sub: begin
-//				output_result = Sub_out;
-//				end
-//			Shr: begin 
-//				Shr_out = A >> 1;
-//				output_result = Shr_out;
-//				end
-//			Shra: begin
-//				Shra_out = A >>> 1;
-//				output_result = Shra_out;
-//				end
-//			Shl: begin
-//				Shl_out = A << 1;
-//				output_result = Shl_out;
-//			Ror: begin
-//				// perform ror
-//				// do we need a module for this?
-//				end
-//			Rol: begin
-//				// rol
-//				end
-			And: begin
-				output_result = And_out;
-				end
-//			Or: begin
-//				Or_out = A || B;
-//				output_result = Or_out;
-//				end
+		case (InPC)
+			1'b1: begin
+			LO <= A+4;
+			end
+			1'b0: begin
+					case (opcode)
+						Add: begin
+							output_result = Add_out;
+							end
+						Sub: begin
+							output_result = Sub_out;
+							end
+						Shr: begin 
+							output_result = Shr_out;
+							end
+						Shra: begin
+							output_result = Shra_out;
+							end
+						Shl: begin
+							output_result = Shl_out;
+						Ror: begin
+							output_result = Ror_out;
+							end
+						Rol: begin
+							output_result = Rol_out;
+							end
+						And: begin
+							output_result = And_out;
+							end
+						Or: begin
+							output_result = Or_out;
+							end
+						Mul: begin
+							LO = Lo;
+							HI = HI;
+							end
+						Div: begin
+							output_result = Or_out;
+							end
+						Not: begin
+							output_result = Not_out;
+							end
+						Neg: begin
+							output_result = Neg_A;
+							end
+					end
+			endcase
+		end
 		endcase
-	end
-endmodule 
+end
+endmodule
