98b311e774b3c2110a51c77acb4d99c9f3732e3f
coleMeeakings <161643805+coleMeeakings@users.noreply.github.com>
2024-02-28 17:19:20 -0500

    Add files via upload

---
+++ not.v
@@ -0,0 +1,26 @@
+// Ripple Carry Adder
+module not(A, Result);
+
+input [31:0] A, B;
+output [31:0] Result;
+
+reg [31:0] Result;
+
+integer i, j;
+
+always@(A)
+	begin
+		
+		for(i = 0; i < 32; i = i + 1)
+		begin
+			if(A[i])
+				begin
+				Result[i]=0;
+				end
+			else 
+				begin
+				Result[i]=1;
+				end
+		end
+	end
+endmodule
