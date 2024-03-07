eee317a36f93fa922af41112a5c8b1625748fa34
Steven Calles <stevencalles@outlook.com>
2024-03-07 10:05:09 -0500

    completed phase 1

--- mul_32.v
+++ mul_32.v
@@ -1,46 +1,47 @@
-module mul_32(input signed [31:0] A, B, output reg signed [31:0] HI, LO);
+module mul_32(input signed [31:0] A, B, output signed [31:0] HI, LO);
+		
+	 reg [2:0] B_G [15:0];
+    reg signed [32:0] PP [15:0];
+	 reg signed [63:0] S_PP [15:0];
+	 wire signed [31:0] n_a;
+	 wire signed [31:0] n_a2;
+	 wire signed [31:0] a_2;
+	 
+	 reg signed [63:0] tempreg;
+	 integer i;
+	 assign n_a = -A;
+	 assign a_2 = A<<1;
+	 assign n_a2 = n_a << 1;
 
-    reg signed [63:0] product;
-    reg [6:0] count;
-    reg [1:0] state;
-    reg [1:0] prev_bit;
-	 reg signed [31:0] shifted_B;
-
-    always @(A, B) begin
-        case (state)
-            2'b00: begin
-                product <= {32'b0, A}; // Initialize product
-                count <= 7'b0; // Initialize counter
-                state <= 2'b01;
-                prev_bit <= 2'b00;
-            end
-            2'b01: begin
-                if (count < 32) begin // Check if all bits have been processed
-                    prev_bit <= B[1:0];
-                    if (B[0] != prev_bit[1]) begin
-                        if (B[0] == 1'b0 && prev_bit[1] == 1'b1) begin
-                            product <= product + A; // Add A to product
-                        end
-                        else if (B[0] == 1'b1 && prev_bit[1] == 1'b0) begin
-                            product <= product - A; // Subtract A from product
-                        end
-                    end
-                    shifted_B <= {B[30:0], B[31]}; // Shift B right by 1 bit
-                    count <= count + 1; // Increment counter
-                    state <= 2'b01;
-                end
-                else begin
-                    state <= 2'b10; // Move to the next state for result extraction
-                end
-            end
-            2'b10: begin
-                HI <= product[63:32]; // Extract HI
-                LO <= product[31:0]; // Extract LO
-                state <= 2'b11; // Move to the final state
-            end
-            default: begin
-                state <= state; // Hold the state
-            end
-        endcase
-    end
+    always @(A or B or n_a) begin
+		B_G[0] = {B[1], B[0], 1'b0};
+			
+		for (i = 1; i < 16; i = i + 1) begin
+        B_G[i] = {B[2*i+1], B[2*i], B[2*i-1]};
+		end
+		
+		for (i = 0; i < 16; i = i+1) begin
+			case(B_G[i])
+			3'b001, 3'b010:
+				PP[i] = {A[31], A};
+			3'b011:
+				PP[i] = {a_2[31], a_2};
+			3'b100:
+				PP[i] = {n_a2[31], n_a2};
+			3'b101, 3'b110:
+				PP[i] = {n_a[31], n_a};
+			default:
+				PP[i] = 0;
+			endcase
+			S_PP[i] = PP[i] << (2*i);
+		end
+		tempreg = S_PP[0];
+		for (i = 1; i < 16; i =i+1) begin
+			tempreg = tempreg + S_PP[i];
+		end
+	end
+	assign HI = tempreg[63:32];
+	assign LO = tempreg[31:0];
 endmodule
+
+
